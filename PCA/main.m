fec6cd850c57bdbfafc98a6c883df6bd34a245d6
zimenglyu <zlyu01@syr.edu>
2019-12-04 16:07:54 -0500

    added eigen faces

--- PCA/main.m
+++ PCA/main.m
@@ -1,4 +1,4 @@
-%% read image
+%% read digit image
 [image,label] = read_hw;
 four=find(label==4);
 images=image(:, four);
@@ -6,16 +6,33 @@ images=image(:, four);
 %% show image
 imshow(reshape(images(:,1),28,28));
 
+%% face image
+data1=read_to_matrix('../att_faces');
+data1=double(data1);
 %% use MATLAB pca
-[coeff,score,latent,tsquared,explained,mu] = pca(images');
+[coeff,score,latent,~,explained,mu] = pca(data1');
 k = variance(explained, 0.7);
 % coeff(1:k,:)*images
 
 
 %% PCA with SVD, each column is a PC
-[v1, eigenvector1]=pca_svd(images);
+[v1, eigenvector1]=pca_svd(data1);
 k1 = variance(v1,0.7);
 
+%% eigen faces
+% eigenfaces1 = eigenvector1 * data1;
+eigenfaces1 = data1 * eigenvector1;
+% eigenfaces1 = eigenvector1' * data1';
+eigenfaces1 = uint8(normalize(eigenfaces1, 0, 255));
+imshow(reshape(eigenfaces1(:,2),112,92))
+
 %% PCA with eigen, each column is a PC
-[v2, eigenvector2]=pca_eigen(images);
+[v2, eigenvector2]=pca_eigen(data1);
 k2 = variance(v2,0.7);
+
+%% eigen faces
+eigenfaces2 = eigenvector2' * data1;
+% eigenfaces2=int(eigenfaces2);
+imshow(reshape(eigenfaces2(:,2),112,92))
+eigenfaces1 = uint8(normalize(eigenfaces1, 0, 255));
+imshow(reshape(eigenfaces1(:,2),112,92))
--- PCA/pca_svd.m
+++ PCA/pca_svd.m
@@ -1,11 +1,12 @@
-function [variances, eigenvector]=pca_svd(images,th)
+function [variances, eigenvector]=pca_svd(images)
 
     avg=mean(images,2);
     [m,n]=size(images);
 
     sub_avg = images-repmat(avg,1,n);
-    Y = sub_avg / sqrt(n-1);
-    [~,s,eigenvector] = svd(Y);
+%     Y = sub_avg' / sqrt(n-1);
+%     [~,s,eigenvector] = svd(Y);
+    [~,s,eigenvector] = svd(sub_avg);
     s_diag=diag(s);
     variances = s_diag.^2;
 %     p = variance(sig2,th);
