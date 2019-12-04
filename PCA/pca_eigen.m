c14d2dd1015894ddd2206c84ada43f10478cb12c
zimenglyu <zlyu01@syr.edu>
2019-12-04 13:44:10 -0500

    fixed a small important bug

--- PCA/pca_eigen.m
+++ PCA/pca_eigen.m
@@ -1,12 +1,12 @@
 function [eigenvalue, eigenvector]=pca_eigen(images)
-% this function doesn't return correct values
+
     avg=mean(images,2);
     [m,n]=size(images);
     sub_avg=images-repmat(avg,1,n);
 %     v=cov(sub_avg');
     covariance = (1 / (n-1)) *( sub_avg * sub_avg');
 %     p=v-covariance;% v=covariance
-    [values, vectors] = eig(covariance);
+    [vectors, values ] = eig(covariance);
     d1=diag(values);
     [eigenvalue, index]=sort(d1,'descend');
     eigenvector=vectors(:,index);
