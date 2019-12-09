f765d1b3c1d1e74edd77e36b395908db81a4c709
Zimeng Lyu <zl7069@rit.edu>
2019-12-09 13:52:56 -0500

    added classification

--- classfication/classification.m
+++ classfication/classification.m
@@ -1,17 +1,26 @@
-
-[trainlabel,traindata] = readtrain(2,1);
-[testlabel,testdata] = readtest(2,1);
-train_y=ind_resp_matx(trainlabel);
+% %%
+% [trainlabel,traindata] = readtrain(2,1);
+% [testlabel,testdata] = readtest(2,1);
+% train_y=ind_resp_matx(trainlabel);
 % z = basic_expension(traindata, 3);
-%%
-y_new=lr_indicator(traindata',train_y',testdata',1);
-% y_new=lr_indicator2(traindata',trainlabel,testdata',1);
-%%
-% y_new=round(y_new);
-% err=test_y-y_new;
-pred=argmax(y_new);
-err=testlabel-pred;
 
-%%
+%% face
+% read images and devide into training and testing data (double)
+[train_data,train_label,test_data,test_label,leftout_data,leftout_label]=get_train_test('../att_faces');
 
-acc_rate=length(find(err==0))/size(testlabel,2);
+train_y=ind_resp_matx(train_label);
+%%
+acc=zeros(1,10);
+for p=1:10
+    y_new=lr_indicator(train_data',train_y',test_data',p);
+    % y_new=lr_indicator2(traindata',trainlabel,testdata',1);
+    
+    % y_new=round(y_new);
+    % err=test_y-y_new;
+    pred=argmax(y_new);
+    err=test_label-pred;
+    
+    acc_rate=length(find(err==0))/size(test_label,2);
+    acc(1,p)=acc_rate;
+end
+plot(acc)
--- classfication/lr_indicator.m
+++ classfication/lr_indicator.m
@@ -1,10 +1,10 @@
 function y_new=lr_indicator(x_train,y_train,x_test,p)
     % steps: 1) get z 2) get w=y*z'(z*z')-1 3) get y_new=w*z
     z_train = basic_expension(x_train, p);
-%     w=z_train\y_train;
-%     w = y_train*z_train*inv(z_train'*z_train)
-    w = (z_train'*z_train)\(z_train'*y_train);
-%     w2= x_train/w'*z_train';
+    w=z_train\y_train;
+
+%     w = (z_train'*z_train)\(z_train'*y_train);
+
     z_test = basic_expension(x_test, p);
     y_new=w'*z_test';
 
